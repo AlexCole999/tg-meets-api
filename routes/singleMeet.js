6120c3cb9615a801100041b4e1b4c553304c0df0
Leo <62186360+AlexCole999@users.noreply.github.com>
2025-07-14 01:07:20 +0500

    user console

--- routes/singleMeet.js
+++ routes/singleMeet.js
@@ -66,7 +66,7 @@ router.post('/single/apply', async (req, res) => {
     }
 
     const user = await User.findOne({ telegramId });
-    console.log('🔍 Найден пользователь:', user)
+    console.log(meet.creator, telegramId, user)
     const name = user?.name || 'Неизвестный пользователь';
 
     await bot.telegram.sendMessage(
