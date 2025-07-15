2d6ad5aa9c7582c9c618e7ca31060e21ff3377c3
Leo <62186360+AlexCole999@users.noreply.github.com>
2025-07-15 20:55:01 +0500

    s3

--- routes/utils.js
+++ routes/utils.js
@@ -1,6 +1,10 @@
+require('dotenv').config(); // 👈 вот это в самом верху
 const express = require('express');
 const router = express.Router();
 const bot = require('../bot');
+
+const { ListObjectsV2Command } = require('@aws-sdk/client-s3');
+const s3 = require('../utils/s3'); // твой файл с конфигом
 
 router.post('/log', async (req, res) => {
   const { userId, message } = req.body;
@@ -14,6 +18,18 @@ router.post('/log', async (req, res) => {
   } catch (err) {
     console.error('❌ Ошибка при отправке:', err);
     res.status(500).send('❌ Не удалось отправить');
+  }
+});
+
+router.get('/test-s3', async (req, res) => {
+  try {
+    const data = await s3.send(new ListObjectsV2Command({
+      Bucket: process.env.S3_BUCKET
+    }));
+    res.json({ status: 'ok', objects: data.Contents });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Ошибка доступа к S3', details: err.message });
   }
 });
 
